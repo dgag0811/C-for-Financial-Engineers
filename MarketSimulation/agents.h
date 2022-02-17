fd24bc3afce53ad552bc59e391ec5be678083c65
Divya Garg <dgag0811@gmail.com>
2022-02-17 21:16:15 +0530

    Update agents.h

--- MarketSimulation/agents.h
+++ MarketSimulation/agents.h
@@ -1,28 +1,27 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cmath>
+#include <chrono>
+#include <algorithm>
 using namespace std;
 
 #ifndef Agent_H
 #define Agent_H
 class Agent {
 public:
-	// the probability the agent trades during a period
-	double tradeProb;
-	// a scaling factor for the size of the trade
-	double tradeScale;
+	double TradeProb;
+	double TradeScale;
 	Agent(double tradeProb, double tradeScale);
 	virtual ~Agent() {};
 	virtual double tick(double price) = 0;
-	virtual void reset() { 
-		tradeProb = 0;
-		tradeScale = 0;
-	};
+	virtual void reset() {};
 };
 #endif 
 
 #ifndef Result_H
 #define Result_H
-class Result{
+class Result {
 public:
 	int sim;
 	int period;
@@ -30,39 +29,42 @@ public:
 	Result() {};
 	Result(int sim, int period, double price) : sim(sim), period(period), price(price) {};
 };
+
 #endif
 
 #ifndef Dealer_H
 #define Dealer_H
 class Dealer {
 private:
-	vector<Agent* > agents;
-	double priceScale;
-public:	
+	vector<Agent*> agents;
+	double PriceScale;
+
+public:
 	Dealer(double priceScale);
 	virtual ~Dealer() {};
 	void addAgent(Agent* a);
-	void runSimulation(int numSimulations, int numPeriods, vector< Result >& simResults);
+
+	void runSimulation(int numSimulations, int numPeriods, vector<Result>& simResults);
 	double getPrice(double net);
 };
 #endif
 
 #ifndef ValueAgent_H
 #define ValueAgent_H
-class ValueAgent : public Agent {
+class ValueAgent :public Agent {
 public:
-	ValueAgent(double tradeProb, double tradeScale) : Agent(tradeProb, tradeScale) {};
+	ValueAgent(double tradeProb, double tradeScale) :Agent(tradeProb, tradeScale) {};
 	double tick(double price);
 };
 #endif
 
 #ifndef TrendAgent_H
 #define TrendAgent_H
-class TrendAgent : public Agent {
+class TrendAgent :public Agent {
 protected:
 	double prevPrice = 1.0;
 public:
-	TrendAgent(double tradeProb, double tradeScale) : Agent(tradeProb, tradeScale) {};
+	TrendAgent(double tradeProb, double tradeScale) :Agent(tradeProb, tradeScale) {};
 	double tick(double price);
 	void reset();
 };
@@ -70,11 +72,10 @@ public:
 
 #ifndef NoiseAgent_H
 #define NoiseAgent_H
-class NoiseAgent : public Agent {
+class NoiseAgent :public Agent {
 public:
-	NoiseAgent(double tradeScale) : Agent(1.0, tradeScale) {}
+	NoiseAgent(double tradeScale) :Agent(1.0, tradeScale) {};
 	double tick(double price);
 };
-
 #endif
 
